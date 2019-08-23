cce5146340c7f4baef52c0918945a0ac9459c628
boris <boris.spivak93@gmail.com>
2019-08-23 15:51:51 +0200

    create data models

--- src/app/core/services/search.service.ts
+++ src/app/core/services/search.service.ts
@@ -2,20 +2,6 @@ import {Injectable} from "@angular/core";
 import {Http, Response} from "@angular/http";
 import {Observable} from "rxjs";
 
-export interface ISearchResultItem  {
-    answer_count: number;
-    closed_date: number;
-    closed_reason: string;
-    creation_date: number;
-    is_answered: boolean;
-    last_activity_date: number;
-    link: string;
-    score: number;
-    tags: Array<string>;
-    title: string;
-    view_count: number;
-}
-
 @Injectable()
 export class SearchService {
 
@@ -26,7 +12,7 @@ export class SearchService {
 
     }
 
-    search(keyword: string): Observable<JSON> {
+    search(keyword: string): Observable<ISoAPIResponse> {
         return this.http.get(SearchService.apiUrl + keyword)
             .map((res: Response) => {
                 let data = res.json();
@@ -36,5 +22,61 @@ export class SearchService {
             .catch((err: Response) => Observable.of(err.json()));
     }
 
-
+export interface ISoOwner {
+  reputation: number,
+  user_id: number,
+  user_type: string,
+  accept_rate: number,
+  profile_image: string,
+  display_name: string,
+  link: string,
 }
+
+export interface ISearchResultItem {
+  tags: Array<string>,
+  owner: ISoOwner,
+  is_answered: boolean,
+  view_count: number,
+  closed_date: number,
+  answer_count: number,
+  score: number,
+  last_activity_date: number,
+  creation_date: number,
+  last_edit_date: number,
+  question_id: number,
+  link: string,
+  closed_reason: string,
+  title: string,
+}
+
+export interface ISoAPIResponse {
+  items: Array<ISearchResultItem>,
+}
+
+export interface IWeatherItem {
+  "Datum": string,
+  "Zeit": string,
+  "Temp. A.": number,
+  "Temp. 3": number,
+  "Feuchte A.": number,
+  "Luftdruck": number,
+  "Regen": number,
+  "Wind": number,
+  "Richtung": number,
+  "Helligkeit": number
+}
+
+/*
+  {
+    "Datum": "",
+    "Zeit": "",
+    "Temp. A.": "°C",
+    "Temp. 3": "°C",
+    "Feuchte A.": "%",
+    "Luftdruck": "",
+    "Regen": "mm",
+    "Wind": "km/h",
+    "Richtung": "°",
+    "Helligkeit": "h"
+  },
+*/
